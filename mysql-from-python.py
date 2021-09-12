dd8c19410244a498416511b34d2c33b80af1b93d
Richard Blaauw <r.t.blaauw@live.com>
2021-09-12 13:28:05 +0000

    Add code that creates table if it doesn't exist

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # Get username from Cloud9 workspace
@@ -13,11 +14,17 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a query
-    with connection.cursor() as cursor:
+    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
         sql = "SELECT * FROM Artist;"
         cursor.execute(sql)
         result = cursor.fetchall()
         print(result)
+        sql = "SELECT * FROM Genre;"
+        cursor.execute(sql)
+        for row in cursor:
+            print(row)
+        cursor.execute(""" CREATE TABLE IF NOT EXISTS
+                       Friends(name char(20), age int, DOB datetime);""")
 finally:
     # Close the connection, regardless of whether or not the above was successful
     connection.close()
